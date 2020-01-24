fff62691c8538a1797e4709afb67a63d2e6f7366
Steven <codemumbler@users.noreply.github.com>
2020-01-23 21:19:29 -0500

    refactor

--- src/main/java/io/github/codemumbler/RomanNumerals.java
+++ src/main/java/io/github/codemumbler/RomanNumerals.java
@@ -6,9 +6,6 @@ public class RomanNumerals {
   private static final Integer[] ROMAN_NUMERALS_VALUES = new Integer[]{ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
   public String convert(int value) {
-    if (value <= 0) {
-      return "";
-    }
     for (int index = 0; index < ROMAN_NUMERALS.length; index++) {
       if (ROMAN_NUMERALS_VALUES[index] <= value) {
         return ROMAN_NUMERALS[index] + convert(value - ROMAN_NUMERALS_VALUES[index]);
