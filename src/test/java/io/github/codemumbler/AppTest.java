7d0870739ba80ceb316164186abbea6218fd77c5
Steven <codemumbler@users.noreply.github.com>
2020-01-23 20:32:02 -0500

    basic java project

---
+++ src/test/java/io/github/codemumbler/AppTest.java
@@ -0,0 +1,13 @@
+package io.github.codemumbler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+  @Test
+  public void testMain() {
+    App app = new App();
+  }
+}
