9e7a5c25278fedc8d83995d8d3ef86d578658b3d
Steven <codemumbler@users.noreply.github.com>
2020-01-23 20:41:48 -0500

    convert 2 to II

--- src/main/java/io/github/codemumbler/RomanNumerals.java
+++ src/main/java/io/github/codemumbler/RomanNumerals.java
@@ -1,8 +1,10 @@
 package io.github.codemumbler;
 
 public class RomanNumerals {
-  
+
   public String convert(int i) {
+    if (i == 2)
+      return "II";
     return "I";
   }
 }
--- src/test/java/io/github/codemumbler/RomanNumeralsTest.java
+++ src/test/java/io/github/codemumbler/RomanNumeralsTest.java
@@ -1,14 +1,26 @@
 package io.github.codemumbler;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsTest {
 
+  private RomanNumerals romanNumerals;
+
+  @BeforeEach
+  public void setUp() {
+    romanNumerals = new RomanNumerals();
+  }
+
   @Test
-  public void convertOneToI() {
-    RomanNumerals romanNumerals = new RomanNumerals();
+  public void convertOne() {
     assertEquals("I", romanNumerals.convert(1));
+  }
+
+  @Test
+  public void convertTwo() {
+    assertEquals("II", romanNumerals.convert(2));
   }
 }
