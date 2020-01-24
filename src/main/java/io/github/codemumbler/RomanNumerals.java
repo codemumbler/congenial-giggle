0c977695d81a2af11693c27c2e0e45a7da0bf72b
Steven <codemumbler@users.noreply.github.com>
2020-01-23 20:57:53 -0500

    refactor

--- src/main/java/io/github/codemumbler/RomanNumerals.java
+++ src/main/java/io/github/codemumbler/RomanNumerals.java
@@ -3,24 +3,21 @@ package io.github.codemumbler;
 public class RomanNumerals {
 
   public String convert(int i) {
-    if (i > 10) {
+    if (i >= 10) {
       return "X" + convert(i - 10);
-    }
-    if (i == 10) {
-      return "X";
     }
     if (i == 9) {
       return "IX";
     }
-    if (i > 5) {
-      return "V" + convert(i-5);
+    if (i >= 5) {
+      return "V" + convert(i - 5);
     }
-    switch (i) {
-      case 5: return "V";
-      case 4: return "IV";
-      case 3: return "III";
-      case 2: return "II";
-      default: return "I";
+    if (i == 4) {
+      return "IV";
     }
+    if (i == 0) {
+      return "";
+    }
+    return "I" + convert(i - 1);
   }
 }
