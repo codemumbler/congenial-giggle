dcf3e2fdb28357b0f3b9562b889ad42f64905f1c
Steven <codemumbler@users.noreply.github.com>
2020-01-25 22:22:51 -0500

    greeter - refactor

--- src/main/java/io/github/codemumbler/Greeter.java
+++ src/main/java/io/github/codemumbler/Greeter.java
@@ -14,18 +14,30 @@ public class Greeter {
     String greeting = HELLO;
     name = name.trim();
     int hourOfTheDay = getCalendar().get(Calendar.HOUR_OF_DAY);
-    if (hourOfTheDay >= 6 && hourOfTheDay < 12) {
+    if (isTheMorning(hourOfTheDay)) {
       greeting = GOOD_MORNING;
     }
-    if (hourOfTheDay >= 18 && hourOfTheDay < 22) {
+    if (isTheEvening(hourOfTheDay)) {
       greeting = GOOD_EVENING;
     }
-    if (hourOfTheDay >= 22 || hourOfTheDay < 6) {
+    if (isTheNight(hourOfTheDay)) {
       greeting = GOOD_NIGHT;
     }
     String fullGreeting = String.format("%s %s%s", greeting, name.substring(0, 1).toUpperCase(), name.substring(1));
     System.out.println(fullGreeting);
     return fullGreeting;
+  }
+
+  private boolean isTheNight(int hourOfTheDay) {
+    return hourOfTheDay >= 22 || hourOfTheDay < 6;
+  }
+
+  private boolean isTheEvening(int hourOfTheDay) {
+    return hourOfTheDay >= 18 && hourOfTheDay < 22;
+  }
+
+  private boolean isTheMorning(int hourOfTheDay) {
+    return hourOfTheDay >= 6 && hourOfTheDay < 12;
   }
 
   void setCalendar(Calendar calendar) {
