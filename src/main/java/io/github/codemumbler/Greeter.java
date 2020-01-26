6e2d0e4c6c0e319c3fa7b34317ff0406bb13e2e4
Steven <codemumbler@users.noreply.github.com>
2020-01-25 22:00:17 -0500

    greeter - good morning

--- src/main/java/io/github/codemumbler/Greeter.java
+++ src/main/java/io/github/codemumbler/Greeter.java
@@ -1,9 +1,30 @@
 package io.github.codemumbler;
 
+import java.util.Calendar;
+
 public class Greeter {
 
+  public static final String HELLO = "Hello";
+  public static final String GOOD_MORNING = "Good morning";
+  private Calendar calendar;
+
   public String greet(String name) {
+    String greeting = HELLO;
     name = name.trim();
-    return String.format("Hello %s%s", name.substring(0, 1).toUpperCase(), name.substring(1));
+    if (getCalendar().get(Calendar.HOUR_OF_DAY) >= 6) {
+      greeting = GOOD_MORNING;
+    }
+    return String.format("%s %s%s", greeting, name.substring(0, 1).toUpperCase(), name.substring(1));
+  }
+
+  void setCalendar(Calendar calendar) {
+    this.calendar = calendar;
+  }
+
+  private Calendar getCalendar() {
+    if (calendar == null) {
+      calendar = Calendar.getInstance();
+    }
+    return this.calendar;
   }
 }
--- src/test/java/io/github/codemumbler/GreeterTest.java
+++ src/test/java/io/github/codemumbler/GreeterTest.java
@@ -3,6 +3,8 @@ package io.github.codemumbler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreeterTest {
@@ -12,6 +14,9 @@ public class GreeterTest {
   @BeforeEach
   public void setUp() {
     greeter = new Greeter();
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.HOUR_OF_DAY, 5);
+    greeter.setCalendar(calendar);
   }
 
   @Test
@@ -27,5 +32,13 @@ public class GreeterTest {
   @Test
   public void greetNameCapitalized() {
     assertEquals("Hello Name", greeter.greet("name"));
+  }
+
+  @Test
+  public void greetInTheMorning() {
+    Calendar calendar = Calendar.getInstance();
+    calendar.set(Calendar.HOUR_OF_DAY, 6);
+    greeter.setCalendar(calendar);
+    assertEquals("Good morning Name", greeter.greet("name"));
   }
 }
