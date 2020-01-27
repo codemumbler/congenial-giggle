d99bb2537d87685e7ec2a110a0214fc27c18f6cd
Steven <codemumbler@users.noreply.github.com>
2020-01-26 21:47:13 -0500

    StringCalculator TDD - empty string

--- src/main/java/io/github/codemumbler/StringCalculator.java
+++ src/main/java/io/github/codemumbler/StringCalculator.java
@@ -3,6 +3,10 @@ package io.github.codemumbler;
 public class StringCalculator {
 
   public int add(String numbers) {
-    return 3;
+    int total = 0;
+    for (String number : numbers.split(",")) {
+      total += (number.isEmpty() ? 0 : Integer.parseInt(number));
+    }
+    return total;
   }
 }
--- src/test/java/io/github/codemumbler/StringCalculatorTest.java
+++ src/test/java/io/github/codemumbler/StringCalculatorTest.java
@@ -1,14 +1,31 @@
 package io.github.codemumbler;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
 
+  private StringCalculator stringCalculator;
+
+  @BeforeEach
+  public void setUp() {
+    stringCalculator = new StringCalculator();
+  }
+
   @Test
-  public void addTwoNumbers() {
-    StringCalculator stringCalculator = new StringCalculator();
+  public void addTwoNumbers_3() {
     assertEquals(3, stringCalculator.add("1,2"));
+  }
+
+  @Test
+  public void addTwoNumbers_2() {
+    assertEquals(2, stringCalculator.add("1,1"));
+  }
+
+  @Test
+  public void addEmptyString() {
+    assertEquals(0, stringCalculator.add(""));
   }
 }
