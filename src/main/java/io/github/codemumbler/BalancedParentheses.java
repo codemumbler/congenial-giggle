4f287f92bde6832004a8b4e91f225be097f3360e
Steven <codemumbler@users.noreply.github.com>
2020-01-27 20:41:30 -0500

    balanced parentheses - unbalanced one

--- src/main/java/io/github/codemumbler/BalancedParentheses.java
+++ src/main/java/io/github/codemumbler/BalancedParentheses.java
@@ -3,6 +3,6 @@ package io.github.codemumbler;
 public class BalancedParentheses {
 
   public boolean isBalanced(String parentheses) {
-    return true;
+    return parentheses.equals("()");
   }
 }
--- src/test/java/io/github/codemumbler/BalancedParenthesesTest.java
+++ src/test/java/io/github/codemumbler/BalancedParenthesesTest.java
@@ -1,14 +1,27 @@
 package io.github.codemumbler;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BalancedParenthesesTest {
 
+  private BalancedParentheses balancedParentheses;
+
+  @BeforeEach
+  public void setUp() {
+    balancedParentheses = new BalancedParentheses();
+  }
+
   @Test
   public void oneSetIsBalanced() {
-    BalancedParentheses balancedParentheses = new BalancedParentheses();
     assertTrue(balancedParentheses.isBalanced("()"));
+  }
+
+  @Test
+  public void notBalanced() {
+    assertFalse(balancedParentheses.isBalanced(")("));
   }
 }
