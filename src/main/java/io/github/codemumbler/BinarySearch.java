9b8d68810d1e1c8cf5fc73f2e532a449e1af5d39
Steven <codemumbler@users.noreply.github.com>
2020-01-28 20:25:01 -0500

    binary search - no element found

--- src/main/java/io/github/codemumbler/BinarySearch.java
+++ src/main/java/io/github/codemumbler/BinarySearch.java
@@ -1,12 +1,19 @@
 package io.github.codemumbler;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 public class BinarySearch<T> {
 
+  private ArrayList<T> list = new ArrayList<>();
+
   public T search(T element) {
-    return element;
+    if (list.contains(element))
+      return element;
+    throw new NoSuchElementException();
   }
 
   public void add(T element) {
-
+    list.add(element);
   }
 }
--- src/test/java/io/github/codemumbler/BinarySearchTest.java
+++ src/test/java/io/github/codemumbler/BinarySearchTest.java
@@ -1,15 +1,38 @@
 package io.github.codemumbler;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BinarySearchTest {
 
+  private BinarySearch<Integer> binarySearch;
+
+  @BeforeEach
+  public void setUp() {
+    binarySearch = new BinarySearch<>();
+  }
+
   @Test
-  public void searchInt() {
-    BinarySearch<Integer> binarySearch = new BinarySearch<>();
+  public void singleInteger() {
     binarySearch.add(5);
     assertEquals(5, binarySearch.search(5));
   }
+
+  @Test
+  public void notFound() {
+    binarySearch.add(5);
+    assertThrows(NoSuchElementException.class, () -> binarySearch.search(1));
+  }
+
+  //  @Test
+  //  public void multipleIntegers() {
+  //    binarySearch.add(1);
+  //    binarySearch.add(3);
+  //    assertEquals(1, binarySearch.search(1));
+  //  }
 }
