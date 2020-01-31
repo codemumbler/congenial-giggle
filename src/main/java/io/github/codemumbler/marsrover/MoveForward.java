160b8b946ca6f6876023f1f128b96a58793b4e0e
Steven <codemumbler@users.noreply.github.com>
2020-01-30 21:45:34 -0500

    mars rover - move forward in all directions

--- src/main/java/io/github/codemumbler/marsrover/MoveForward.java
+++ src/main/java/io/github/codemumbler/marsrover/MoveForward.java
@@ -4,6 +4,16 @@ public class MoveForward implements MarsRoverStrategy {
 
   @Override
   public Vector execute(Vector coordinates) {
-    return new Vector(coordinates.getX(), coordinates.getY() + 1, coordinates.getDirection());
+    switch (coordinates.getDirection()) {
+      case N:
+        return new Vector(coordinates.getX(), coordinates.getY() + 1, coordinates.getDirection());
+      case S:
+        return new Vector(coordinates.getX(), coordinates.getY() - 1, coordinates.getDirection());
+      case W:
+        return new Vector(coordinates.getX() - 1, coordinates.getY(), coordinates.getDirection());
+      default:
+        return new Vector(coordinates.getX() + 1, coordinates.getY(), coordinates.getDirection());
+    }
+
   }
 }
